cbc5a713598316ef881fc87eb07caa5943f9bb98
chaudhuree <chaudhuree00@gmail.com>
2023-08-22 17:02:16 +0600

    services section done

--- nike practice/src/components/index.js
+++ nike practice/src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCards.jsx";
+import ServiceCard from "./ServiceCard.jsx";
 
-export { Button, Nav, ShoeCard, PopularProductCard };
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard };
